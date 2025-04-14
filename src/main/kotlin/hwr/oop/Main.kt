25265d203e39dcc71f2226e1487deca0b274cd0f
dev-laky <laky.dev@proton.me>
2025-04-14 12:38:55 +0200

    sample: add InitCommand and Poker command structure with tests

--- src/main/kotlin/hwr/oop/Main.kt
+++ src/main/kotlin/hwr/oop/Main.kt
@@ -1,6 +1,12 @@
 package hwr.oop
 
-fun main() {
-  val answer = KotlinExample().sayHello()
-  println(answer)
-}
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.subcommands
+import hwr.oop.commands.InitCommand
+import hwr.oop.commands.Poker
+
+fun main(args: Array<String>) = Poker()
+    .subcommands(
+        InitCommand(),
+    )
+    .main(args)
