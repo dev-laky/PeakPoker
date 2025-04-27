7ad2abe01ce2f0854a7b43e315f56b6e9194329b
dev-laky <laky.dev@proton.me>
2025-04-27 11:58:54 +0200

    feat(commands): add database commands and tests for poker game / moved logic classes to own folders

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/Player.kt
+++
@@ -1,7 +0,0 @@
-package hwr.oop.projects.peakpoker.core
-
-class Player(
-    val name: String,
-){
-// TODO: Player needs to be implemented
-}
---
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/player/Player.kt
@@ -0,0 +1,5 @@
+package hwr.oop.projects.peakpoker.core.player
+
+class Player() {
+    // TODO: Implement player class
+}
