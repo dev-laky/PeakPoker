0f12f92d7761857b0e3945640548f19f10e04018
dev-laky <laky.dev@proton.me>
2025-04-27 12:25:49 +0200

    feat(player): resolve sonarcube issues

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/player/Player.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/player/Player.kt
@@ -1,5 +1,44 @@
 package hwr.oop.projects.peakpoker.core.player
 
-class Player() {
-    // TODO: Implement player class
+import hwr.oop.projects.peakpoker.core.card.Card
+
+class Player(
+    val name: String,
+    private var chips: Int = 0,
+    private var hand: List<Card> = emptyList(),
+    private var bet: Int = 0,
+    private var isFolded: Boolean = false,
+    private var isAllIn: Boolean = false
+) {
+    val currentBet: Int get() = bet
+    val currentChips: Int get() = chips
+    val currentHand: List<Card> get() = hand.toList()
+
+    fun raiseBet(amount: Int) {
+        when {
+            amount < 0 -> throw IllegalArgumentException("Bet amount must be positive")
+            isFolded -> throw IllegalStateException("Cannot raise bet after folding")
+            isAllIn -> throw IllegalStateException("Cannot raise bet after going all-in")
+        }
+        bet += amount
+        chips -= amount
+    }
+
+    fun fold() {
+        isFolded = true
+    }
+
+    fun isFolded(): Boolean {
+        return isFolded
+    }
+
+    fun allIn() {
+        isAllIn = true
+    }
+
+    fun isAllIn(): Boolean {
+        return isAllIn
+    }
+
+    // TODO: Implement the hand functionality and think about it's logic
 }
