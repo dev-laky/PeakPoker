c7d369f29a40b2111fd9cfbcbe8184bd1a2bd308
Julian Kücken <101923513+juliankuecken@users.noreply.github.com>
2025-05-05 17:56:58 +0200

    Update src/test/kotlin/hwr/oop/projects/peakpoker/core/deck/DeckTest.kt

--- src/test/kotlin/hwr/oop/projects/peakpoker/core/deck/DeckTest.kt
+++ src/test/kotlin/hwr/oop/projects/peakpoker/core/deck/DeckTest.kt
@@ -1,7 +1,9 @@
 package hwr.oop.projects.peakpoker.core.deck
 
 import io.kotest.core.spec.style.AnnotationSpec
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotSame
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.assertThrows
 
 class DeckTest : AnnotationSpec() {
