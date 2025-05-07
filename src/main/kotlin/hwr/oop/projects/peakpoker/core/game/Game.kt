19ee995a0324d3f64599e72eea714404799570b7
Julian Kücken <julian.kuecken@icloud.com>
2025-05-07 14:47:06 +0200

    refactor: standardize variable naming in Game class for consistency

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/game/Game.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/game/Game.kt
@@ -3,7 +3,7 @@ import hwr.oop.projects.peakpoker.core.card.Card
 import hwr.oop.projects.peakpoker.core.player.Player
 
 class Game (
-    val ID: Int, // GameManager automatically generates GameID and passes it on Game-creation
+    val id: Int, // GameManager automatically generates GameID and passes it on Game-creation
     // Both values are set by GameManager and passed on Game-creation
     val smallBlindAmount: Int,
     val bigBlindAmount: Int
@@ -14,7 +14,7 @@ class Game (
         require(bigBlindAmount >= smallBlindAmount) { "Big blind amount must be greater than or equal to small blind amount" }
     }
 
-    val PlayersOnTable: MutableList<Player> = mutableListOf()
+    val playersOnTable: MutableList<Player> = mutableListOf()
     val pot: Int = 0
 
     val communityCards: List<Card> = emptyList()
@@ -34,41 +34,41 @@ class Game (
 
     // FIXME: Does not work yet since currentPlayerIndex is not set/modified correctly.
     fun getCurrentPlayer(): Player {
-        return PlayersOnTable[currentPlayerIndex]
+        return playersOnTable[currentPlayerIndex]
     }
 
     // FIXME: Does not work yet since currentPlayerIndex is not set/modified correctly.
     fun getHighestBet(): Int {
-        return PlayersOnTable[currentPlayerIndex-1].getBetAmount()
+        return playersOnTable[currentPlayerIndex-1].getBetAmount()
     }
 
-    fun getPot(): Int {
-        return pot
-    }
+//    fun getPot(): Int {
+//        return pot
+//    }
 
     fun calculatePot(): Int {
-        return PlayersOnTable.sumOf { it.getBetAmount() }
+        return playersOnTable.sumOf { it.getBetAmount() }
     }
 
     fun getPlayersListAsString(): String {
-        return PlayersOnTable.joinToString(", ") { it.name }
+        return playersOnTable.joinToString(", ") { it.name }
     }
 
     fun checkPlayerValidity(player: Player): Boolean {
-        return PlayersOnTable.none { it.name == player.name }
+        return playersOnTable.none { it.name == player.name }
     }
 
     fun addPlayer(player: Player) {
         if (!checkPlayerValidity(player)) {
             throw IllegalArgumentException("Player with name ${player.name} already exists.")
         } else {
-            PlayersOnTable.add(player)
+            playersOnTable.add(player)
         }
     }
 
     fun removePlayer(player: Player) {
-        if (PlayersOnTable.contains(player)) {
-            PlayersOnTable.remove(player)
+        if (playersOnTable.contains(player)) {
+            playersOnTable.remove(player)
         } else {
             throw IllegalArgumentException("Player ${player.name} does not exist.")
         }
