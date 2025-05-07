cc6f96677fd4f15406ac263138600f34926ae7aa
Testspieler09 <pepehanisch06@gmail.com>
2025-05-07 07:43:05 +0200

    feat(tests): add a test for the main function

--- src/main/kotlin/hwr/oop/projects/peakpoker/Main.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/Main.kt
@@ -1,17 +1,11 @@
 package hwr.oop.projects.peakpoker
 
-import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.parse
 import com.github.ajalt.clikt.core.subcommands
 import hwr.oop.projects.peakpoker.commands.PokerCommand
 import hwr.oop.projects.peakpoker.commands.db.DbCommand
 import hwr.oop.projects.peakpoker.commands.db.DbConnect
 import hwr.oop.projects.peakpoker.commands.db.DbInit
 
-fun main(args: Array<String>) = PokerCommand()
-    .subcommands(
-        DbCommand().subcommands(
-            DbInit(),
-            DbConnect()
-        )
-    )
-    .main(args)
+fun main(args: Array<String>) =
+    PokerCommand().subcommands(DbCommand().subcommands(DbInit(), DbConnect())).parse(args)
---
+++ src/test/kotlin/hwr/oop/projects/peakpoker/MainTest.kt
@@ -0,0 +1,16 @@
+package hwr.oop.projects.peakpoker
+
+import com.github.ajalt.clikt.core.CliktError
+import io.kotest.core.spec.style.AnnotationSpec
+import org.assertj.core.api.Assertions.assertThatNoException
+import org.junit.jupiter.api.assertThrows
+
+class MainTest : AnnotationSpec() {
+
+    @Test
+    fun `test main function execution without errors`() {
+        // Test execution - use `parse` for non-exiting invocation
+        assertThatNoException().isThrownBy { main(arrayOf("db", "init")) }
+        assertThrows<CliktError> { main(arrayOf("nonesense")) }
+    }
+}
