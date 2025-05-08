95171ea3464dbb825fff85abacca1b1562f71588
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2025-05-08 11:49:56 +0200

    Update HandEvaluatorTest.kt
    Changes imports Ranks* to usual imports

--- src/test/kotlin/hwr/oop/projects/peakpoker/core/hand/HandEvaluatorTest.kt
+++ src/test/kotlin/hwr/oop/projects/peakpoker/core/hand/HandEvaluatorTest.kt
@@ -7,8 +7,8 @@ import hwr.oop.projects.peakpoker.core.card.Card
 import hwr.oop.projects.peakpoker.core.card.CommunityCards
 import hwr.oop.projects.peakpoker.core.card.HoleCards
 import hwr.oop.projects.peakpoker.core.card.Rank
-import hwr.oop.projects.peakpoker.core.card.Suit.*
-import hwr.oop.projects.peakpoker.core.card.Rank.*
+import hwr.oop.projects.peakpoker.core.card.Suit
+import hwr.oop.projects.peakpoker.core.card.Rank
 import hwr.oop.projects.peakpoker.core.card.Suit
 import hwr.oop.projects.peakpoker.core.hand.HandEvaluator
 import hwr.oop.projects.peakpoker.core.hand.HandRank
