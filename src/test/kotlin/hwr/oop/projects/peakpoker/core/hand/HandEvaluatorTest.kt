08bb489caafafa85678a56581a52e4c48f8d2e26
Timwiddo <timwid.tw@gmail.com>
2025-05-08 12:10:14 +0200

    updated imports due to issue

--- src/test/kotlin/hwr/oop/projects/peakpoker/core/hand/HandEvaluatorTest.kt
+++ src/test/kotlin/hwr/oop/projects/peakpoker/core/hand/HandEvaluatorTest.kt
@@ -4,15 +4,11 @@ import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
 import hwr.oop.projects.peakpoker.core.card.Card
-import hwr.oop.projects.peakpoker.core.card.CommunityCards
-import hwr.oop.projects.peakpoker.core.card.HoleCards
 import hwr.oop.projects.peakpoker.core.card.Rank
 import hwr.oop.projects.peakpoker.core.card.Suit.*
 import hwr.oop.projects.peakpoker.core.card.Rank.*
 import hwr.oop.projects.peakpoker.core.card.Suit
-import hwr.oop.projects.peakpoker.core.hand.HandEvaluator
-import hwr.oop.projects.peakpoker.core.hand.HandRank
-import hwr.oop.projects.peakpoker.core.player.PlayerInterface
+
 
 class HandEvaluatorTest : AnnotationSpec() {
     //instanciating cards for testing
