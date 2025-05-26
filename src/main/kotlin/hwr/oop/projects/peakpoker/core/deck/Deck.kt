101c5cacb08847a698cebd5da810b455c59e16e8
Julian Kücken <julikuecken@gmail.com>
2025-05-26 15:11:45 +0200

    feat: replace IllegalStateException with InsufficientCardsException in draw method and update tests

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/deck/Deck.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/deck/Deck.kt
@@ -4,7 +4,6 @@ import hwr.oop.projects.peakpoker.core.card.Card
 import hwr.oop.projects.peakpoker.core.card.Rank
 import hwr.oop.projects.peakpoker.core.card.Suit
 import hwr.oop.projects.peakpoker.core.exceptions.InsufficientCardsException
-import hwr.oop.projects.peakpoker.core.exceptions.InsufficientChipsException
 
 class Deck() {
   // Create list of all possible cards and shuffle it right away
@@ -34,7 +33,7 @@ class Deck() {
    * @throws InsufficientCardsException If there aren't enough cards left in the deck
    */
   fun draw(amount: Int = 1): List<Card> {
-    check(cards.size >= amount) { throw InsufficientChipsException("Not enough cards left in the deck") }
+    check(cards.size >= amount) { throw InsufficientCardsException("Not enough cards left in the deck") }
     val drawnCards = mutableListOf<Card>()
 
     repeat(amount) {
