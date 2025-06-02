106fbea6baa14fffe26865337a01d4f1bfd25afa
Emre Dirlik <sloovie95@gmail.com>
2025-06-02 15:10:47 +0200

    refactor: make properties private and update access methods for Game, GameId, HoleCards, and Player classes

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameId.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameId.kt
@@ -1,9 +1,18 @@
 package hwr.oop.projects.peakpoker.core.game
 
-data class GameId(val value: String) {
+data class GameId(private val value: String) {
+
+  fun getValue(): String {
+    return value
+  }
+
   companion object {
     fun generate(): GameId {
       return GameId(java.util.UUID.randomUUID().toString())
     }
   }
+
+    override fun toString(): String {
+        return value
+    }
 }
