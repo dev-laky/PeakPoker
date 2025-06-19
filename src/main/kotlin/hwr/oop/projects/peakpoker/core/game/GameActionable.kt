d90a2d84b04f7f3d287f357e3ad6e5a8333aeeba
dev-laky <laky.dev@proton.me>
2025-06-19 13:00:00 +0200

    chore(game): optimized imports

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameActionable.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameActionable.kt
@@ -1,7 +1,5 @@
 package hwr.oop.projects.peakpoker.core.game
 
-import hwr.oop.projects.peakpoker.core.player.PokerPlayer
-
 interface GameActionable {
   fun raiseBetTo(playerName: String, chips: Int)
   fun call(playerName: String)
