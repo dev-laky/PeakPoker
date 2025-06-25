411ca5b8759aefce068fc8ead66564d0b9981f7e
dev-laky <laky.dev@proton.me>
2025-06-25 17:58:19 +1000

    chore(commands): deleted obsolete commands

--- src/main/kotlin/hwr/oop/projects/peakpoker/commands/game/GameCommand.kt
+++
@@ -1,7 +0,0 @@
-package hwr.oop.projects.peakpoker.commands.game
-
-import com.github.ajalt.clikt.core.CliktCommand
-
-class GameCommand : CliktCommand(name = "game") {
-  override fun run() = Unit
-}
