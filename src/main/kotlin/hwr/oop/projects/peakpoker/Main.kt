ad22a329820aff223093ae2235fef4eca0da2fa8
dev-laky <laky.dev@proton.me>
2025-06-25 17:54:08 +1000

    feat(main): included `persistenceAdapter`

--- src/main/kotlin/hwr/oop/projects/peakpoker/Main.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/Main.kt
@@ -3,19 +3,23 @@ package hwr.oop.projects.peakpoker
 import com.github.ajalt.clikt.core.CliktError
 import com.github.ajalt.clikt.core.parse
 import com.github.ajalt.clikt.core.subcommands
-import hwr.oop.projects.peakpoker.commands.PokerCommand
-import hwr.oop.projects.peakpoker.commands.db.DbCommand
-import hwr.oop.projects.peakpoker.commands.db.DbConnect
-import hwr.oop.projects.peakpoker.commands.db.DbInit
-import hwr.oop.projects.peakpoker.commands.game.GameCommand
-import hwr.oop.projects.peakpoker.commands.game.GameNew
-import hwr.oop.projects.peakpoker.commands.hand.HandCommand
+import java.io.File
 
 fun main(args: Array<String>) {
-  val command = PokerCommand().subcommands(
-    DbCommand().subcommands(
-      DbInit(),
-      DbConnect()
+  val persistenceAdapter =
+    hwr.oop.projects.peakpoker.persistence.FileSystemPersistenceAdapter(
+      File("poker_data.json")
+    )
+
+  val command = Poker().subcommands(
+    CreateNewGame(persistenceAdapter),
+    HandInfo(persistenceAdapter),
+    GameInfo(persistenceAdapter),
+    Raise(persistenceAdapter, persistenceAdapter),
+    Call(persistenceAdapter, persistenceAdapter),
+    Check(
+      persistenceAdapter,
+      persistenceAdapter
     ),
     GameCommand().subcommands(
       GameNew()
