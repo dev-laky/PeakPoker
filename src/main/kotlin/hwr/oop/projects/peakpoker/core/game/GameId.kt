e34ec2f12dc2572f95c43e994844722c82cfff89
dev-laky <laky.dev@proton.me>
2025-06-25 17:39:37 +1000

    refactor(game): implement serialization for GameActionable and GameId, enhance PokerGame with game state management

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameId.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/game/GameId.kt
@@ -1,5 +1,8 @@
 package hwr.oop.projects.peakpoker.core.game
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GameId(val value: String) {
   companion object {
     fun generate(): GameId {
