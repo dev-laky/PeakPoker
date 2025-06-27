068c1641dde27ffae750d32b9627737f9e906a73
juliankuecken <julikuecken@gmail.com>
2025-06-27 08:44:23 +0300

    chore(format): apply formatter

--- src/main/kotlin/hwr/oop/projects/peakpoker/core/card/HoleCards.kt
+++ src/main/kotlin/hwr/oop/projects/peakpoker/core/card/HoleCards.kt
@@ -6,25 +6,26 @@ import kotlinx.serialization.Transient
 
 @Serializable
 class HoleCards(
-    val cards: List<Card>,
-    @Transient val player: PokerPlayer? = null,
+  val cards: List<Card>,
+  @Transient val player: PokerPlayer? = null,
 ) : Iterable<Card> by cards {
-    /**
-     * Exception thrown when duplicate cards are found in the hole cards
-     */
-    class DuplicateCardException(message: String) : IllegalStateException(message)
+  /**
+   * Exception thrown when duplicate cards are found in the hole cards
+   */
+  class DuplicateCardException(message: String) : IllegalStateException(message)
 
-    /**
-     * Exception thrown when the number of hole cards is invalid
-     */
-    class InvalidCardConfigurationException(message: String) : IllegalStateException(message)
+  /**
+   * Exception thrown when the number of hole cards is invalid
+   */
+  class InvalidCardConfigurationException(message: String) :
+    IllegalStateException(message)
 
-    init {
-        if (cards.isNotEmpty() && cards.size != 2) {
-            throw InvalidCardConfigurationException("Hole cards must be empty or contain exactly two cards.")
-        }
-        if (cards.distinct().size != cards.size) {
-            throw DuplicateCardException("Hole cards must not contain duplicates.")
-        }
+  init {
+    if (cards.isNotEmpty() && cards.size != 2) {
+      throw InvalidCardConfigurationException("Hole cards must be empty or contain exactly two cards.")
     }
+    if (cards.distinct().size != cards.size) {
+      throw DuplicateCardException("Hole cards must not contain duplicates.")
+    }
+  }
 }
